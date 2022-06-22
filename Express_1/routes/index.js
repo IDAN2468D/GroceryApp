94a0b81bc505e28ed708d26451c405efbf668250
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2022-06-22 14:11:12 +0300

    Update

--- Express_1/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.json({ msg: "express work perfect 12:31" })
-})
-
-module.exports = router;
